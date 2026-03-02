ef72386c0aab610561f044c341dba8e856877f3c
Carlos Adriano Lourenço da Silva <carlosad16@hotmail.com>
2026-03-01 22:00:27 -0300

    Create page.js

---
+++ app/dashboard/admin/page.js
@@ -0,0 +1,15 @@
+"use client"
+
+export default function Admin() {
+  return (
+    <div>
+      <h2>Painel da Escola</h2>
+      <p>Você é Admin 🏫</p>
+      <ul>
+        <li>Gerenciar professores</li>
+        <li>Gerenciar alunos</li>
+        <li>Ver relatórios</li>
+      </ul>
+    </div>
+  )
+}
