5500e4c90188494a18e6e76e6e641b1c45b7c783
Carlos Adriano Lourenço da Silva <carlosad16@hotmail.com>
2026-03-01 22:02:08 -0300

    Create page.js

---
+++ app/dashboard/professor/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+export default function Professor() {
+  return (
+    <div>
+      <h2>Painel do Professor</h2>
+      <p>Você é Professor 📚</p>
+      <ul>
+        <li>Lançar OCR</li>
+        <li>Ver alunos</li>
+      </ul>
+    </div>
+  )
+}
