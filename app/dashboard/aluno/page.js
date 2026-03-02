07f4d98cb4fb00f50631382c6721b9ed9656183e
Carlos Adriano Lourenço da Silva <carlosad16@hotmail.com>
2026-03-01 22:03:36 -0300

    Create page.js

---
+++ app/dashboard/aluno/page.js
@@ -0,0 +1,13 @@
+"use client"
+
+export default function Aluno() {
+  return (
+    <div>
+      <h2>Área do Aluno</h2>
+      <p>Bem-vindo 👨‍🎓</p>
+      <ul>
+        <li>Ver resultados</li>
+      </ul>
+    </div>
+  )
+}
