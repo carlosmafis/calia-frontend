4ce13919f560dedbc5da355b31cefa7491e7d573
Carlos Adriano Lourenço da Silva <carlosad16@hotmail.com>
2026-03-01 21:59:47 -0300

    Create page.js

---
+++ app/dashboard/super-admin/page.js
@@ -0,0 +1,15 @@
+"use client"
+
+export default function SuperAdmin() {
+  return (
+    <div>
+      <h2>Painel Global</h2>
+      <p>Você é Super Admin 👑</p>
+      <ul>
+        <li>Gerenciar escolas</li>
+        <li>Controlar planos</li>
+        <li>Visualizar métricas globais</li>
+      </ul>
+    </div>
+  )
+}
